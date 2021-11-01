538a6401d66d6e2b0e1497018ab2c813cedeab39
ali bozna <alibozna@gmail.com>
2021-11-01 20:48:32 +0100

    Add SDL sources and binaries with custom pre-build.

--- sources/loader/entry/main.cpp
+++ sources/loader/entry/main.cpp
@@ -1,8 +1,28 @@
 #include <iostream>
 
+#include <SDL.h>
+
+#ifdef main
+#undef main
+#endif
+
 
 int main()
 {
-    std::cout << "Hello\n";
+    SDL_Init(SDL_INIT_VIDEO);
+
+    auto window = SDL_CreateWindow("Vault7", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_OPENGL);
+
+    if (!window)
+    {
+        return -1;
+    }
+
+
+    SDL_Delay(3000);
+
+    SDL_DestroyWindow(window);
+    SDL_Quit();
+
     return 0;
 }
